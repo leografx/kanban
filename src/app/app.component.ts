4b8de864c833ff40654439b60ff0a69ad1a9fa60
dps <dpsImaci5@dps-iMac.local>
2017-05-10 16:07:04 -0400

    WIP filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit} from '@angular/core';
 import { Http,RequestOptions,Request,RequestMethod } from '@angular/http';
 import { Dps } from './data/jobs'
+import { Observable } from 'rxjs/observable'
 import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers:[Dps]
+  providers:[Dps],
 })
 export class AppComponent implements OnInit {
   title = 'DPS';
-  jobs:any[] = [];
+  jobs:any = [];
   prepress:any[] = [];
   ryobi:any[] = [];
+  data='';
 
   
   constructor(private dps:Dps){
@@ -22,6 +24,12 @@ export class AppComponent implements OnInit {
 
   ngOnInit(){
     //console.log(this.jobs);
+  }
+
+  filter(data){
+     console.log(data);
+     this.jobs.filter( el => console.log(el.job_no === data));
+     //console.log(this.jobs);
   }
 
 
