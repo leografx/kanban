e8c646e36b59a4f5e6af24da2ebaaa95470f8ab2
dps <dpsImaci5@dps-iMac.local>
2017-05-10 14:15:56 -0400

    Kanban board master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Http,RequestOptions,Request,RequestMethod } from '@angular/http';
+import { Dps } from './data/jobs'
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[Dps]
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  title = 'DPS';
+  jobs:any[] = [];
+  prepress:any[] = [];
+  ryobi:any[] = [];
+
+  
+  constructor(private dps:Dps){
+  	this.jobs = this.dps.jobs;
+  }
+
+  ngOnInit(){
+    //console.log(this.jobs);
+  }
+
+
 }
